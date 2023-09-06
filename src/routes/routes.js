9ceb307f9f1496ad429cd36b7b8dfa95d913f1ff
jocabo95 <camargojl1995@gmail.com>
2023-09-06 10:42:51 -0500

    added importation in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import CartContainer from "../components/pages/cart/CartContainer";
 import CheckoutContainer from "../components/pages/checkout/CheckoutContainer";
 import Dashboard from "../components/pages/dashboard/Dashboard";
+import ItemDetailContainer from "../components/pages/itemDetail/ItemDetailContainer";
 import ItemListContainer from "../components/pages/itemList/ItemListContainer";
 
 export const routes =[
