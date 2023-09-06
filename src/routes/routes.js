1697ed8a9bff5bfb80b8d43961eb4ac9bc622783
jocabo95 <camargojl1995@gmail.com>
2023-09-06 15:13:00 -0500

    changed paths order

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,36 +4,36 @@ import Dashboard from "../components/pages/dashboard/Dashboard";
 import ItemDetailContainer from "../components/pages/itemDetail/ItemDetailContainer";
 import ItemListContainer from "../components/pages/itemList/ItemListContainer";
 
-export const routes =[
-    {
-        id: "home",
-        path: "/",
-        // Element tiene que tener caps en primera para ser identificado como component
-        Element: ItemDetailContainer
-    },
-    {
-        id: "cart",
-        path: "/cart",
-        Element: CartContainer
-    },
-    {
-        id: "category",
-        path: "/category/:name",
-        Element: ItemListContainer
-    },
-    {
-        id: "itemdetail",
-        path: "/detail/:id",
-        Element: ItemDetailContainer
-    },
-    {
-        id: "checkout",
-        path: "/checkout",
-        Element: CheckoutContainer
-    },
-    {
-        id: "dashboard",
-        path: "/dashboard",
-        Element: Dashboard
-    }
-]
+export const routes = [
+  {
+    id: "home",
+    path: "/",
+    // Element tiene que tener caps en primera para ser identificado como component
+    Element: ItemListContainer,
+  },
+  {
+    id: "cart",
+    path: "/cart",
+    Element: CartContainer,
+  },
+  {
+    id: "itemdetail",
+    path: "/detail/:id",
+    Element: ItemDetailContainer,
+  },
+  {
+    id: "category",
+    path: "/category/:name",
+    Element: ItemListContainer,
+  },
+  {
+    id: "checkout",
+    path: "/checkout",
+    Element: CheckoutContainer,
+  },
+  {
+    id: "dashboard",
+    path: "/dashboard",
+    Element: Dashboard,
+  },
+];
